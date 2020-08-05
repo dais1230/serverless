52c0646a2aed66cbb10dd2054445dcd816a3d050
daisuke13 <d.katsuta1230@gmail.com>
2020-08-05 18:15:05 +0300

    added redux-persist for keeping stored data when refreshing

--- front/src/index.js
+++ front/src/index.js
@@ -9,14 +9,27 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../src/reducers/index'
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer, applyMiddleware(thunk))
+let persistor = persistStore(store)
 
 ReactDOM.render(
   <React.StrictMode>
       <AppProvider i18n={ja}>
         <Provider store={store}>
-          <App />
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
         </Provider>
       </AppProvider>
   </React.StrictMode>,
